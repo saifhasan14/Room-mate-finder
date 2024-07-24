91036a44f4180eba4a1adae55d4215fc96bb59d6
saifhasan14 <binal0818@gmail.com>
2024-07-24 17:02:26 +0530

    slider and list page completed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,47 @@
-import Navbar from "./components/navbar/Navbar";
-import "./layout.scss";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom"
+import ListPage from "./routes/listPage/listPage";
+import Layout from "./routes/layout/layout";
 import HomePage from "./routes/homePage/homePage";
+import SinglePage from "./routes/singlePage/singlePage";
+
 
 function App() {
+  
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout/>,
+      children: [
+        {
+          path: "/",
+          element: <HomePage/>
+        },
+        {
+          path: "/list",
+          element: <ListPage/>
+        },
+        {
+          path: "/:id",
+          element: <SinglePage/>
+        },
+      ]
+    }
+  ]);
 
   return (
-    <div className="layout">
-      <Navbar/>
-      <HomePage/>
-    </div>
+    // <div className="layout">
+    // <div className="navbar">
+    //   <Navbar/>
+    // </div>
+    // <div className="content">
+    //   <HomePage/>
+    // </div>
+    // </div>
+    <RouterProvider router={router}/>
   )
 }
 
